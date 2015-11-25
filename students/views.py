60e201dc63e6988bbdf6b43838879a4fd18daaad
IvanT <ivan.tarasenko@oracle.com>
2015-11-25 19:09:13 +0200

    KZ 5 fix2

--- students/views.py
+++ students/views.py
@@ -5,9 +5,10 @@ from students.models import Student
 def list_view(request):
     if request.GET.get('course_id'):
 	students=Student.objects.filter(courses=request.GET.get('course_id'))
+	return render(request, 'students/list_view.html', {'students': students, 'course_id' : request.GET.get('course_id') })
     else:
 	students=Student.objects.all()
-    return render(request, 'students/list_view.html', {'students': students})    
+	return render(request, 'students/list_view.html', {'students': students})    
 
 def detail(request, student_id):
     return render(request, 'students/detail.html', { 'student' : Student.objects.get(id=student_id)})
