143443ce8c8b92bcf77b1f48c839fa9527a025cb
IvanT <ivan.tarasenko@oracle.com>
2015-12-02 17:48:12 +0200

    KZ_7_full hf5

--- quadratic/views.py
+++ quadratic/views.py
@@ -6,21 +6,24 @@ from quadratic.forms import QuadraticForm
 def quadratic_results(request):
     context={}
     if request.method == "GET" and request.GET.get('a') != None and request.GET.get('b') != None and request.GET.get('b') != None:
-	context['form'] = QuadraticForm(request.GET)
-	if context['form'].is_valid():
-	    a = context['form'].cleaned_data['a']
-	    b = context['form'].cleaned_data['b']
-	    c = context['form'].cleaned_data['c']
+	form = QuadraticForm(request.GET)
+	if form.is_valid():
+	    a = form.cleaned_data['a']
+	    b = form.cleaned_data['b']
+	    c = form.cleaned_data['c']
 	    # claculating discriminant
-	    context['d'] = d = int(b ** 2 - 4 * a * c)
+	    d = int(b ** 2 - 4 * a * c)
 	    if d < 0:
-		context['result'] = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+		result = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
 	    elif d == 0:
-		context['result'] = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %0.1f" % (-b / 2 * a)
+		result = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %0.1f" % (-b / 2 * a)
 	    else:
 		x1 = (-b + d ** (1/2.0)) / (2 * a)
 		x2 = (-b - d ** (1/2.0)) / (2 * a)
-		context['result'] = "Квадратное уравнение имеет два действительных корня: x1 = %0.1f, x2 = %0.1f" % (x1, x2)
+		result = "Квадратное уравнение имеет два действительных корня: x1 = %0.1f, x2 = %0.1f" % (x1, x2)
+	    context['d'] = d
+	    context['result'] = result
+	context['form'] = form
     else:
 	context['form'] = QuadraticForm()
     return render(request, "quadratic/results.html",  context )
