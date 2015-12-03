f1c4ac455dd95a76ddc26bf6a24a2b94cd203461
IvanT <ivan.tarasenko@oracle.com>
2015-12-03 18:26:15 +0200

    KZ 7.1 Another try

--- students/views.py
+++ students/views.py
@@ -8,10 +8,10 @@ from django.contrib import messages
 def list_view(request):
     if request.GET.get('course_id'):
 	students = Student.objects.filter(courses = request.GET.get('course_id'))
-	return render(request, 'students/list_view.html', {'students': students, 'course_id' : request.GET.get('course_id') })
+	return render(request, 'students/list.html', {'students': students, 'course_id' : request.GET.get('course_id') })
     else:
 	students = Student.objects.all()
-	return render(request, 'students/list_view.html', {'students': students})    
+	return render(request, 'students/list.html', {'students': students})    
 
 def detail(request, student_id):
     return render(request, 'students/detail.html', { 'student' : Student.objects.get(id = student_id)})
