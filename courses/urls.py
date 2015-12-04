79136529a76c0a7e3ab989c351576a7f8cfa28c2
IvanT <ivan.tarasenko@oracle.com>
2015-12-04 18:48:35 +0200

    KZ 8 initial attempt to fix#5_wtf

--- courses/urls.py
+++ courses/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^add/$', views.add, name = "add"),
     url(r'^edit/(?P<course_id>[0-9]+)/$', views.edit, name = "edit"),
     url(r'^remove/(?P<course_id>[0-9]+)/$', views.remove, name = "remove"),
-    url(r'^(?P<course_id>[0-9]+)/add_lesson/$', views.add_lesson, name = "add-lesson")
+    url(r'^(?P<course_id>[0-9]+)/add_lesson$', views.add_lesson, name = "add-lesson")
 )
